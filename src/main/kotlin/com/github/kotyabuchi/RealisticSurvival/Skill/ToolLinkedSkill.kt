6d08046a83fd66248cbf03765fb3f569c8069e34
kotyabuchi <kotyabuchi@gmail.com>
2021-10-12 14:34:38 +0900

    Add: ツール連携式のスキルインターフェースを追加

---
+++ src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/Skill/ToolLinkedSkill.kt
@@ -0,0 +1,70 @@
+package com.github.kotyabuchi.RealisticSurvival.Skill
+
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.PlayerDropItemEvent
+import org.bukkit.event.player.PlayerItemBreakEvent
+import org.bukkit.event.player.PlayerItemHeldEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.inventory.ItemStack
+import java.util.*
+
+interface ToolLinkedSkill: ActiveSkill {
+
+    val skillItemBackup: MutableMap<UUID, ItemStack>
+
+    @EventHandler
+    fun onClickItem(event: InventoryClickEvent) {
+        val player = event.whoClicked as? Player ?: return
+        if (!isEnabledSkill(player)) return
+        if (event.clickedInventory == null) return
+        if (event.slot != player.inventory.heldItemSlot) return
+        event.isCancelled = true
+        disableSkill(player)
+    }
+
+    @EventHandler
+    fun onBreakItem(event: PlayerItemBreakEvent) {
+        val player = event.player
+        if (isEnabledSkill(player)) {
+            skillItemBackup.remove(player.uniqueId)
+            disableSkill(player)
+        }
+    }
+
+    @EventHandler
+    fun onChangeItemSlot(event: PlayerItemHeldEvent) {
+        val player = event.player
+        if (isEnabledSkill(player)) {
+            disableSkill(player)
+        }
+    }
+
+    @EventHandler
+    fun onDropItem(event: PlayerDropItemEvent) {
+        val player = event.player
+        if (isEnabledSkill(player)) {
+            player.inventory.setItemInMainHand(event.itemDrop.itemStack)
+            event.itemDrop.remove()
+            disableSkill(player)
+        }
+    }
+
+    @EventHandler
+    fun onDeath(event: PlayerDeathEvent) {
+        val player = event.entity
+        if (isEnabledSkill(player)) {
+            disableSkill(player)
+        }
+    }
+
+    @EventHandler
+    fun onLogout(event: PlayerQuitEvent) {
+        val player = event.player
+        if (isEnabledSkill(player)) {
+            disableSkill(player)
+        }
+    }
+}
