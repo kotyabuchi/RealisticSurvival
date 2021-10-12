54b823d67185ec75b38b7d99dfa1e3b5239c357e
kotyabuchi <kotyabuchi@gmail.com>
2021-10-12 16:13:47 +0900

    Change: PlayerInteractBlockEventからBlockとEquipmentSlotをNullSafeで取得できるように変更

--- src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/Event/PlayerInteractBlockEvent.kt
+++ src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/Event/PlayerInteractBlockEvent.kt
@@ -8,5 +8,12 @@ import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
 
-class PlayerInteractBlockEvent(player: Player, action: Action, itemStack: ItemStack?, block: Block, blockFace: BlockFace, equipmentSlot: EquipmentSlot): PlayerInteractEvent(player, action, itemStack, block, blockFace, equipmentSlot) {
+class PlayerInteractBlockEvent(player: Player, action: Action, itemStack: ItemStack?, private val block: Block, blockFace: BlockFace, private val equipmentSlot: EquipmentSlot): PlayerInteractEvent(player, action, itemStack, block, blockFace, equipmentSlot) {
+    override fun getClickedBlock(): Block {
+        return block
+    }
+
+    override fun getHand(): EquipmentSlot {
+        return equipmentSlot
+    }
 }
