39339f8d3738e5fb336ab998591bb9dc0059d2a0
kotyabuchi <kotyabuchi@gmail.com>
2021-10-19 16:51:39 +0900

    Add: メインとなるメニューを追加

---
+++ src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/Menu/MainMenu.kt
@@ -0,0 +1,10 @@
+package com.github.kotyabuchi.RealisticSurvival.Menu
+
+import net.kyori.adventure.text.Component
+
+class MainMenu: Menu(Component.text("Menu"), 1) {
+
+    init {
+        setFrame()
+    }
+}
