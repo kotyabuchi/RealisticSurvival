01f730c334777aecf0dd4558a9e38c441449eda6
kotyabuchi <kotyabuchi@gmail.com>
2021-10-29 15:17:49 +0900

    Change: エンダーパールを消費することでコストが半減するように変更

--- src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/Menu/MenuButton/Home/HomeInfoButton.kt
+++ src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/Menu/MenuButton/Home/HomeInfoButton.kt
@@ -5,30 +5,38 @@ import com.github.kotyabuchi.RealisticSurvival.Menu.MenuButton.MenuButton
 import com.github.kotyabuchi.RealisticSurvival.System.Player.Home
 import com.github.kotyabuchi.RealisticSurvival.System.Player.getStatus
 import com.github.kotyabuchi.RealisticSurvival.Utility.Emoji
+import com.github.kotyabuchi.RealisticSurvival.Utility.consume
 import com.github.kotyabuchi.RealisticSurvival.Utility.normalize
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.Particle
 import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
 import kotlin.math.round
 import kotlin.random.Random
 
 class HomeInfoButton(val player: Player, val home: Home): MenuButton() {
     private val location = Location(home.world, home.x, home.y, home.z, home.yaw, 0f)
     private val distanceCost = round(player.location.toVector().distance(location.toVector()) / 3).toInt()
-    private var cost = if (home.world != location.world) 100 + distanceCost else distanceCost
+    private var cost = distanceCost
+    private var pearCost = distanceCost / 2
 
     init {
+        if (home.world != player.world) {
+            cost += 100
+            pearCost += 100
+        }
         val lore = mutableListOf<Component>()
         lore.add(Component.text("World: ${location.world?.name}").normalize())
         lore.add(Component.text("X: ${location.x}").normalize())
         lore.add(Component.text("Y: ${location.y}").normalize())
         lore.add(Component.text("Z: ${location.z}").normalize())
         lore.add(Component.text("Yaw: ${location.yaw}").normalize())
-        lore.add(Component.text("Cost: ").normalize().append(Component.text(Emoji.DIAMOND + cost).normalize(NamedTextColor.AQUA)))
+        lore.add(Component.text("Cost: ").normalize().append(Component.text("${Emoji.DIAMOND}$cost($pearCost)").normalize(NamedTextColor.AQUA)))
         lore.add(Component.empty())
         lore.add(Component.text("Left Click: Teleport to location").normalize(NamedTextColor.GOLD))
         lore.add(Component.text("Right Click: Remove home").normalize(NamedTextColor.RED))
@@ -39,8 +47,11 @@ class HomeInfoButton(val player: Player, val home: Home): MenuButton() {
         val player = event.whoClicked as? Player ?: return
         val playerStatus = player.getStatus()
         val world = player.world
+        val inv = player.inventory
+        val teleportCost = if (inv.contains(ItemStack(Material.ENDER_PEARL))) pearCost else cost
 
-        if (playerStatus.decreaseMana(cost)) {
+        if (playerStatus.decreaseMana(teleportCost)) {
+            inv.consume(ItemStack(Material.ENDER_PEARL))
             playerStatus.refreshManaIndicator()
             playerStatus.closeMenu()
             for (i in 0 until 20) {
@@ -65,7 +76,7 @@ class HomeInfoButton(val player: Player, val home: Home): MenuButton() {
                 player.sendMessage(Component.text("テレポートに失敗しました").normalize(NamedTextColor.RED))
             }
         } else {
-            player.sendMessage(Component.text("マナが足りません").normalize())
+            player.sendMessage(Component.text("マナが足りません").normalize(NamedTextColor.RED))
         }
     }
 }
