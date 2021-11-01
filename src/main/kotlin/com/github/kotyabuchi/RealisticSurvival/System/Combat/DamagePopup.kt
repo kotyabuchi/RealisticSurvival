24883ada14619198ac7b4c1f77d369572d6e2e37
kotyabuchi <kotyabuchi@gmail.com>
2021-11-01 22:32:14 +0900

    change: 表示する値を0.01以上に変更

--- src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/System/Combat/DamagePopup.kt
+++ src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/System/Combat/DamagePopup.kt
@@ -1,6 +1,6 @@
 package com.github.kotyabuchi.RealisticSurvival.System.Combat
 
-import com.github.kotyabuchi.RealisticSurvival.Utility.floor1Digits
+import com.github.kotyabuchi.RealisticSurvival.Utility.floor2Digits
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Material
@@ -22,31 +22,22 @@ object DamagePopup: Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onDamage(event: EntityDamageEvent) {
         if (event.isCancelled) return
-        if (event.damage <= 0.0) return
+        if (event.damage <= 0.01) return
         val entity = event.entity as? LivingEntity ?: return
-        popup(entity, event.finalDamage.floor1Digits(), true)
+        popup(entity, event.finalDamage.floor2Digits(), true)
     }
 
     @EventHandler
     fun onRegen(event: EntityRegainHealthEvent) {
         if (event.isCancelled) return
-        if (event.amount <= 0.0) return
+        if (event.amount <= 0.01) return
         val entity = event.entity as? LivingEntity ?: return
-        popup(entity, event.amount.floor1Digits(), false)
+        popup(entity, event.amount.floor2Digits(), false)
     }
 
     private fun popup(entity: LivingEntity, amount: Double, isDamage: Boolean) {
         if (entity !is Player) {
-            var needPopup = false
-            val nearEntities = entity.getNearbyEntities(30.0, 30.0, 30.0)
-            for (i in 0 until nearEntities.size) {
-                val checkTarget = nearEntities[i]
-                if (checkTarget is Player) {
-                    needPopup = true
-                    break
-                }
-            }
-            if (!needPopup) return
+            if (entity.world.getNearbyEntitiesByType(Player::class.java, entity.location, 30.0).isEmpty()) return
         }
         val eyeHeight = entity.eyeHeight
         val baseLoc = entity.location.add(.0, eyeHeight, .0)
@@ -54,6 +45,7 @@ object DamagePopup: Listener {
         val y = Random.nextInt(ceil((eyeHeight / 4.0) * 10).toInt()) / 10.0
         val z = Random.nextInt(15) / 10.0 - .75
         val popupLoc = baseLoc.add(x, y, z)
+        val color = if (isDamage) NamedTextColor.RED else NamedTextColor.GREEN
         entity.world.spawnEntity(popupLoc, EntityType.AREA_EFFECT_CLOUD).apply {
             this as AreaEffectCloud
             duration = 30
@@ -63,7 +55,7 @@ object DamagePopup: Listener {
             radiusPerTick = 0f
             setParticle(Particle.BLOCK_CRACK, Material.AIR.createBlockData())
             isCustomNameVisible = true
-            customName(Component.text(amount, if (isDamage) NamedTextColor.RED else NamedTextColor.GREEN))
+            customName(Component.text(amount, color))
         }
     }
 }
