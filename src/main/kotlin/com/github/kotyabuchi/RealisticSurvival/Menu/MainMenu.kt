9730577e8b3ff54be70a5324d32983beb67fa140
kotyabuchi <kotyabuchi@gmail.com>
2021-11-04 15:31:25 +0900

    Change: OPプレイヤー時の判定用引数を追加

--- src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/Menu/MainMenu.kt
+++ src/main/kotlin/com/github/kotyabuchi/RealisticSurvival/Menu/MainMenu.kt
@@ -4,7 +4,7 @@ import com.github.kotyabuchi.RealisticSurvival.Menu.MenuButton.Home.HomeButton
 import com.github.kotyabuchi.RealisticSurvival.Menu.MenuButton.Job.JobButton
 import net.kyori.adventure.text.Component
 
-class MainMenu: Menu(Component.text("Menu"), 2, FrameType.TOP, FrameType.SIDE) {
+class MainMenu(private val isOp: Boolean): Menu(Component.text("Menu"), 3, FrameType.TOP, FrameType.SIDE) {
 
     init {
         createMenu()
